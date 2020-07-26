71aa0fe7be20aa6680b74de80398284e19786503
EverStarck <EverStarckOne@protonmail.com>
2020-07-25 20:19:11 -0500

    work with github official example

---
+++ src/components/Switcher.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Switcher = () => {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>World</h2>
+    </>
+  );
+};
+
+export default Switcher;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// import i18n (needs to be bundled ;))
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
